cb05d6996c89f21d8c5c715160f302b7f15281b6
A7442 <parkjuna1121@naver.com>
2025-05-11 02:14:22 +0900

    feat : 게임 클리어, 게임 오버 추가

--- AngelAndDevil/Assets/Scripts/Core/UI/GameUIController.cs
+++ AngelAndDevil/Assets/Scripts/Core/UI/GameUIController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameUIController : BaseUIController
 {
     public Canvas menuCanvas;
     public Canvas soundCanvas;
+    public Canvas clearCanvas;
+    public Canvas gameOverCanvas;
     public TMP_Text timeText;
     public TMP_Text angelHostageText;
     public TMP_Text devilHostageText;
+    public TMP_Text FinaltimeText;
 
     private float time = 0f;//건우님하고 조율하기
 
@@ -18,6 +22,9 @@ public class GameUIController : BaseUIController
     private int maxDevilHostage = 3;//test용
     [SerializeField]private int angelHostage = 1;//test용
     [SerializeField]private int devilHostage = 2;//test용
+
+    [SerializeField]private bool clear = false;//test용
+    [SerializeField]private bool isGameOver = false;//test용
     protected override void Awake()
     {
         base.Awake();
@@ -26,12 +33,28 @@ public class GameUIController : BaseUIController
     {
         menuCanvas.gameObject.SetActive(false);
         soundCanvas.gameObject.SetActive(false);
+        clearCanvas.gameObject.SetActive(false);
+        gameOverCanvas.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        UpdateTimeText();
-        UpdateHostageNumber();
+        if (clear)
+        {
+            Time.timeScale = 0f;
+            FinaltimeText.text = timeText.text;
+            clearCanvas.gameObject.SetActive(true);
+        }
+        else if (isGameOver)
+        {
+            Time.timeScale = 0f;
+            gameOverCanvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            UpdateTimeText();
+            UpdateHostageNumber();
+        }
     }
     protected override UIState GetUIState()
     {
@@ -67,6 +90,13 @@ public class GameUIController : BaseUIController
             CloseUI(menuCanvas.gameObject);
             Time.timeScale = 1f;
         }
+    }
+
+    public void Retry(Button button)
+    {
+        isGameOver = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(button.name);
     }
 
     private void UpdateTimeText()
